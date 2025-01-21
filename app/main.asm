05cce1edc09423813cfc079513d965d184c1969f
Peter Buckley <peterabbit22.22@gmail.com>
2025-01-21 10:54:21 -0700

    Deleted unessesary code

--- app/main.asm
+++ app/main.asm
@@ -28,36 +28,7 @@ SetupP1:
         bis.b   #BIT0,&P1DIR            ; P1.0 output
         bic.w   #LOCKLPM5,&PM5CTL0      ; Unlock I/O pins
 
-Mainloop:
-        jmp     Delay                   ; Jump to delay subroutine
-        jmp     Timer                   ; Jump to timer subroutine
-
-
-;-------------------------------------------------------------------------------
-; Delay Loop
-;-------------------------------------------------------------------------------
-Delay:
-        xor.b   #BIT0, &P1OUT          ; Toggle P1.0 every 0.5s
-        mov.w   #2, R14                ; Outer loop count
-OuterLoop:
-        mov.w   #65000, R15            ; Inner loop count (0.5 second period maxes out 16bit register)
-InnerLoop:
-        dec.w   R15                    ; Decrement R15
-        jnz     InnerLoop              ; Inner loop done?
-        dec.w   R14                    ; Decrement R14
-        jnz     OuterLoop              ; Outer loop done?
-
-        mov.w   #44000, R15            ; Remaining cycles
-InnerLoop2:
-        dec.w   R15                    ; Decrement R15
-        jnz     InnerLoop2             ; Finish remaining cycles
-
-        jmp     Delay                  ; Repeat the loop
-
-;-------------------------------------------------------------------------------
-; Timer Setup and loop
-;-------------------------------------------------------------------------------
-Timer:		
+SetupHeatbeatTimer:		
         ; Setup Timer TB0
 		bis.w	#TBCLR, &TB0CTL
 		bis.w	#TBSSEL__ACLK, &TB0CTL
@@ -71,8 +42,10 @@ Timer:
 
 		; Enable global interrupts
 		bis.w	#GIE, SR
-L2:
-        jmp     L2                      ; Infinite Loop
+
+
+Main:
+        jmp     Main                   ; Jump to timer subroutine
 
 ;-------------------------------------------------------------------------------
 ; Interrupt Service Routines
